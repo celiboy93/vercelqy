fdfa95404ec245410581bc1f9f5feec9c0cb7f58
celiboy93 <97116174+celiboy93@users.noreply.github.com>
2026-03-28 16:31:49 +0630

    Update and rename add.js to add.mjs

--- api/add.mjs
+++ api/add.mjs
@@ -1,6 +1,6 @@
-const { getAllLinks, setLink } = require("../lib/maintenance");
+import { getAllLinks, setLink } from "../lib/maintenance.mjs";
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
@@ -22,4 +22,4 @@ module.exports = async function handler(req, res) {
   }
 
   return res.status(200).json({ success: true });
-};
+}
